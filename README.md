840ebed3b58bdc3918b7676423478966a09296e6
AndreyDodonov <Nordhummer@gmail.com>
2020-02-10 12:41:38 +0300

    [dev]init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Travel landing page
+
+Этот лендинг сделан по макету из фигмы [ссылка на макет](https://www.figma.com/file/ClPSP7KCU1NbvxMXA914hlFk/travel-landing-page-jacobvoyles?node-id=0%3A1 "ссылка на фигму")
+в рамках тренировки работы с figma.  
+При разработке задействованы flex, css-grid и даже добавлена некоторая адаптивность =))
---
+++ script/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(function () {
+    // add background to header start
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active');
+        } else if (window.pageYOffset <= 50) {
+            header.classList.remove('header_active');
+        }
+    };
+    // add background to header end
+
+})();
+
+// burger handler
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav_active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+    })
+
+})();
